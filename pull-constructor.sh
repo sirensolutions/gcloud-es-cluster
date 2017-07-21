2e9650e30c859b70e4b37c49c9204668c4296865
Andrew Gallagher <andrewg@andrewg.com>
2017-07-21 17:22:12 +0100

    added pull-constructor.sh

---
+++ pull-constructor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# pull-constructor.sh; invoke by appending the following to `gcloud compute instance create`:
+#   --metadata-from-file startup-script=./gcloud-es-cluster/pull-constructor.sh
+#
+# NB this script is invoked from the *controller* node
+
+git clone https://github.com/sirensolutions/gcloud-es-cluster && ./gcloud-es-cluster/constructor.sh
