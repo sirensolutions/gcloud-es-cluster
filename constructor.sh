d7ef813c23a8e2669764fc6c8da0f50106fe9f9e
Andrew Gallagher <andrewg@andrewg.com>
2017-07-21 17:20:41 +0100

    added constructor.sh

---
+++ constructor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Constructor for setting up an elasticsearch cluster
+
+# Do nothing for now; testing purposes only!
+echo "gcloud-es-cluster/constructor.sh woz ere" | logger -t gcloud-es-cluster
