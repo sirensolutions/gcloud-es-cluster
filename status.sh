817b1309bc96c17ed3c19895278e099203b4d2bc
Andrew Gallagher <andrew.gallagher@siren.io>
2017-07-26 11:26:01 +0100

    Backslash backslash backslash ... backslash? D'oh!

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@
 
 ES_PORT=9200
 
-all_nodes=$(gcloud compute instances list | egrep ^\\S-node\\d+ | awk '{ print $1 }')
+all_nodes=$(gcloud compute instances list | egrep ^\\\S-node | awk '{ print $1 }')
 
 cluster_list=""
 for node in $all_nodes; do
@@ -17,7 +17,7 @@ unique_clusters=$(echo $cluster_list|sort -u)
 for cluster in $unique_clusters; do
 	# get the slave ip from 'describe' rather than 'list' because 'list'
 	# returns empty columns sometimes, making parsing difficult
-	first_slave=$(gcloud compute instances list | egrep ^$cluster-node\\d+ | head -1 | awk '{ print $1 }')
+	first_slave=$(gcloud compute instances list | egrep ^${cluster}-node | head -1 | awk '{ print $1 }')
 	ip=$(gcloud compute instances describe $first_slave|grep networkIP|awk '{print $2}')
 	curl -XGET http://$ip:$ES_PORT/_cluster/state?pretty
 done
