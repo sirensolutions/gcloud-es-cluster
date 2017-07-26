6d74b3e35a1f239ebddcaec08fa844c9c2619ddc
Andrew Gallagher <andrew.gallagher@siren.io>
2017-07-26 14:38:11 +0100

    Fixed output

--- status.sh
+++ status.sh
@@ -22,7 +22,7 @@ for cluster in $unique_clusters; do
 	# get the slave ip from 'describe' rather than 'list' because 'list'
 	# returns empty columns sometimes, making parsing difficult
 	slaves=$(gcloud compute instances list | egrep ^${cluster}-node | awk '{ print $1 }')
-	echo "Found cluster: $cluster ($slaves)"
+	echo "Found cluster: $cluster ($(echo $slaves|wc -l) nodes)"
 	if [[ $1 == "-v" ]]; then
 		first_slave=${slaves%%\ *}
 		ip=$(gcloud compute instances describe $first_slave|grep networkIP|awk '{print $2}')
