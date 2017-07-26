fda4e7d79495e2736532c2186e84c9ad4e0abebc
Andrew Gallagher <andrew.gallagher@siren.io>
2017-07-26 10:59:05 +0100

    Killer usage

--- killer.sh
+++ killer.sh
@@ -2,5 +2,13 @@
 
 # A QAD script to kill all nodes in a named cluster
 
+if [[ ! $1 || $2 ]]; then
+	cat <<EOF
+Usage: $0 <cluster-name>
+
+Deletes every node of the form <cluster-name>-node*
+EOF
+fi
+
 nodes=$(gcloud compute instances list | grep ^$1-node | awk '{ print $1 }')
 gcloud compute instances delete -q $nodes
