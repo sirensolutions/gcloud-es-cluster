62eb057fe23518ab195a293587fe82e15b04a2ff
Andrew Gallagher <andrew.gallagher@siren.io>
2017-07-26 13:42:12 +0100

    Fixed nesting

--- status.sh
+++ status.sh
@@ -15,14 +15,14 @@ done
 IFS="
 " unique_clusters=$(echo "${cluster_list[*]}"|sort -u)
 
-if [[ $1 == "-v" ]]; then
-  for cluster in $unique_clusters; do
+for cluster in $unique_clusters; do
 	# get the slave ip from 'describe' rather than 'list' because 'list'
 	# returns empty columns sometimes, making parsing difficult
 	slaves=$(gcloud compute instances list | egrep ^${cluster}-node | awk '{ print $1 }')
 	echo "Found cluster: $cluster ($slaves)"
-	first_slave=${slaves%%\ *}
-	ip=$(gcloud compute instances describe $first_slave|grep networkIP|awk '{print $2}')
-	curl -XGET http://$ip:$ES_PORT/_cluster/state?pretty
-  done
-fi
+	if [[ $1 == "-v" ]]; then
+		first_slave=${slaves%%\ *}
+		ip=$(gcloud compute instances describe $first_slave|grep networkIP|awk '{print $2}')
+		curl -XGET http://$ip:$ES_PORT/_cluster/state?pretty
+	fi
+done
