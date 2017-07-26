1ad766d0cab7bf0bfc459eb65ab73a664b70a0f6
Andrew Gallagher <andrew.gallagher@siren.io>
2017-07-26 11:13:32 +0100

    Added explanatory note

--- status.sh
+++ status.sh
@@ -15,6 +15,8 @@ done
 
 unique_clusters=$(echo $cluster_list|sort -u)
 for cluster in $unique_clusters; do
+	# get the slave ip from 'describe' rather than 'list' because 'list'
+	# returns empty columns sometimes, making parsing difficult
 	first_slave=$(gcloud compute instances list | egrep ^$cluster-node\\d+ | head -1 | awk '{ print $1 }')
 	ip=$(gcloud compute instances describe $first_slave|grep networkIP|awk '{print $2}')
 	curl -XGET http://$ip:$ES_PORT/_cluster/state?pretty
