ec8bca6a2c58ab6a3e20bc024a89f551659e5cd4
Andrew Gallagher <andrew.gallagher@siren.io>
2017-08-01 09:50:28 +0100

    Added wait for slaves to come up

--- hes-installer.sh
+++ hes-installer.sh
@@ -7,5 +7,9 @@
 
 ansible $1 -u root -m template -a "src=hes-autosetup.template dest=/autosetup"
 ansible $1 -u root -m command -a "installimage"
+
+echo "Waiting for each slave to come back up..."
+ansible $1 -c local -m wait_for -a "port=22"
+
 ansible $1 -u root -m template -a "src=baremetal-puller.template dest=/tmp/puller.sh"
 ansible $1 -u root -m command -a "bash /tmp/puller.sh"
