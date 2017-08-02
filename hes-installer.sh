8cac0bd7f98d3313ec33d1c96323760b903200a7
Andrew Gallagher <andrew.gallagher@siren.io>
2017-08-02 11:54:07 +0100

    Really did fix variables this time

--- hes-installer.sh
+++ hes-installer.sh
@@ -6,8 +6,9 @@
 # as a group in /etc/ansible/hosts
 # Machines will take their names from /etc/ansible/hosts
 
+CLUSTER=$1
 PRIMARY_IP=$(hostname --fqdn)
-SLAVES=$(ansible $1 -c local -m command -a "echo {{ inventory_hostname }}" | grep -v ">>" )
+SLAVES=$(ansible $CLUSTER -c local -m command -a "echo {{ inventory_hostname }}" | grep -v ">>" )
  
 # First, let's populate a hash with our passwords
 # Ansible's password caching is unusable, so we do this ourselves
@@ -24,14 +25,14 @@ for slave in $SLAVES; do
 done
 
 echo "Configure the OS"
-ansible $1 -u root -m template -a "src=hes-autosetup.template dest=/autosetup"
-ansible $1 -u root -m command -a "installimage && reboot"
+ansible $CLUSTER -u root -m template -a "src=hes-autosetup.template dest=/autosetup"
+ansible $CLUSTER -u root -m command -a "installimage && reboot"
 
 # Delete entries from our known_hosts because the keys will have changed
-ansible $1 -c local -m command -a "ssh-keygen -f $HOME/.ssh/known_hosts -R {{ inventory_hostname }}"
+ansible $CLUSTER -c local -m command -a "ssh-keygen -f $HOME/.ssh/known_hosts -R {{ inventory_hostname }}"
 
 echo "Waiting for each slave to come back up..."
-ansible $1 -c local -m wait_for -a "port=22"
+ansible $CLUSTER -c local -m wait_for -a "port=22"
 
 echo "Repopulate root's authorized_keys in the new base OS"
 for slave in $SLAVES; do
@@ -47,8 +48,8 @@ DEBUG=1
 SUBNETS="${PRIMARY_IP}/32 {% for host in groups['$CLUSTER'] %} {{ hostvars[host]['ansible_eth0']['ipv4']['address'] }}/32 {% endfor %}"
 CLUSTER_NAME=$CLUSTER
 EOF
-ansible $1 -u root -m template -b -a "src=${supplement} dest=/tmp/baremetal.conf.supplement"
+ansible $CLUSTER -u root -m template -b -a "src=${supplement} dest=/tmp/baremetal.conf.supplement"
 rm ${supplement}
 
-ansible $1 -u root -m copy -b -a "src=baremetal-puller.sh dest=/tmp/puller.sh"
-ansible $1 -u root -m command -b -a "bash /tmp/puller.sh"
+ansible $CLUSTER -u root -m copy -b -a "src=baremetal-puller.sh dest=/tmp/puller.sh"
+ansible $CLUSTER -u root -m command -b -a "bash /tmp/puller.sh"
