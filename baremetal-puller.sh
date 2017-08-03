f8f989a9dfe2189e4d317ce986c43997614f5182
Andrew Gallagher <andrew.gallagher@siren.io>
2017-08-03 12:36:10 +0100

    Optimised apt-get for headless

--- baremetal-puller.sh
+++ baremetal-puller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp
 
-apt update && apt install git
+apt-get update && apt-get install -y git
 
 if ! git clone https://github.com/sirensolutions/gcloud-es-cluster |& logger -t es-puller; then
 	echo "Aborting; no git repository found" |& logger -t es-puller
