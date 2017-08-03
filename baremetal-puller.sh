fceb4f8c7556a784d6f13c9e1c1ba678ea4f3233
Andrew Gallagher <andrew.gallagher@siren.io>
2017-08-03 12:24:14 +0100

    Can't assume baremetal comes with git preinstalled!

--- baremetal-puller.sh
+++ baremetal-puller.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /tmp
+
+apt update && apt install git
+
 if ! git clone https://github.com/sirensolutions/gcloud-es-cluster |& logger -t es-puller; then
 	echo "Aborting; no git repository found" |& logger -t es-puller
 fi
