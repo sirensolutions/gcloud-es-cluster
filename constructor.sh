ab073f54ec666149af7821fce0b417dafa6f8753
Andrew Gallagher <andrew.gallagher@siren.io>
2018-07-06 15:44:54 +0100

    Canonicalised whitespace

--- constructor.sh
+++ constructor.sh
@@ -3,45 +3,45 @@
 # Constructor for setting up an elasticsearch cluster
 
 check_error() {
-  if [ $? -ne 0 ]; then
-    echo Failed on $1
-    exit 1
-  fi
+    if [ $? -ne 0 ]; then
+        echo Failed on $1
+        exit 1
+    fi
 }
 
 # Function to add quote-brackets to bare ipv6 addresses only
 # Use this before appending ":port" to ensure ipv6 sanity
 bracketed_ip() {
-  ip=$1
-  # We test for an ipv6 address by trying to remove two colons
-  ip_noopenbracket="${ip#[}"
-  ip_removeonecolon="${ip%:*}"
-  ip_removetwocolons="${ip_removeonecolon%:*}"
-  if [[ ${ip_noopenbracket} == ${ip} && ${ip_removetwocolons} != ${ip_removeonecolon} ]]; then
-    # promote bare-ipv6 to bracketed format
-    echo "[${ip}]"
-  else
-    echo "$ip"
-  fi
+    ip=$1
+    # We test for an ipv6 address by trying to remove two colons
+    ip_noopenbracket="${ip#[}"
+    ip_removeonecolon="${ip%:*}"
+    ip_removetwocolons="${ip_removeonecolon%:*}"
+    if [[ ${ip_noopenbracket} == ${ip} && ${ip_removetwocolons} != ${ip_removeonecolon} ]]; then
+        # promote bare-ipv6 to bracketed format
+        echo "[${ip}]"
+    else
+        echo "$ip"
+    fi
 }
 
 # Function to remove quote-brackets and :port from ip addresses
 bare_ip() {
-  ip=$1
-  # We test for an ipv6 address by trying to remove two colons
-  ip_noopenbracket="${ip#[}"
-  ip_removeonecolon="${ip%:*}"
-  ip_removetwocolons="${ip_removeonecolon%:*}"
-  if [[ ${ip_noopenbracket} != ${ip} ]]; then
-    # return bracketed-ipv6 to bare format without any trailing port
-    echo "${ip_noopenbracket%]*}"
-  elif [[ ${ip_noopenbracket} == ${ip} && ${ip_removetwocolons} != ${ip_removeonecolon} ]]; then
-    # do nothing; we have a bare ipv6 (which can't have a :port in principle)
-    echo "$ip"
-  else
-    # remove any trailing port from ipv4
-    echo "${ip%:*}"
-  fi
+    ip=$1
+    # We test for an ipv6 address by trying to remove two colons
+    ip_noopenbracket="${ip#[}"
+    ip_removeonecolon="${ip%:*}"
+    ip_removetwocolons="${ip_removeonecolon%:*}"
+    if [[ ${ip_noopenbracket} != ${ip} ]]; then
+        # return bracketed-ipv6 to bare format without any trailing port
+        echo "${ip_noopenbracket%]*}"
+    elif [[ ${ip_noopenbracket} == ${ip} && ${ip_removetwocolons} != ${ip_removeonecolon} ]]; then
+        # do nothing; we have a bare ipv6 (which can't have a :port in principle)
+        echo "$ip"
+    else
+        # remove any trailing port from ipv4
+        echo "${ip%:*}"
+    fi
 }
 
 # Function to send a nonsense request to the http_proxy for debugging purposes
@@ -76,9 +76,9 @@ ES_LINKNAME=elasticsearch
 total_mem_kb=$(grep MemTotal /proc/meminfo|awk '{print $2}')
 half_mem_mb=$[total_mem_kb / 2048]
 if [[ $half_mem_mb -gt 31744 ]]; then
-  ES_HEAP_SIZE=31744m
+    ES_HEAP_SIZE=31744m
 else
-  ES_HEAP_SIZE=${half_mem_mb}m
+    ES_HEAP_SIZE=${half_mem_mb}m
 fi
 
 # Use default ports for simplicity
@@ -109,12 +109,12 @@ echo DEBUG=$DEBUG
 
 systemdstat=$(systemctl --version | head -1)
 if [[ $? && "$(echo $systemdstat | awk '{print $2}')" -gt 227 ]]; then
-	SYSTEMD=true
+    SYSTEMD=true
 fi
 
 DEPENDENCIES="unzip ufw oracle-java8-installer"
 if [[ ! $SYSTEMD ]]; then
-	DEPENDENCIES="$DEPENDENCIES supervisor"
+    DEPENDENCIES="$DEPENDENCIES supervisor"
 fi
 
 if [[ $http_proxy ]]; then
@@ -141,19 +141,19 @@ fi
 
 ES_MAJOR_VERSION=${ES_VERSION%%.*}
 if [[ $DEBUG ]]; then
-	echo ES_MAJOR_VERSION=$ES_MAJOR_VERSION
+    echo ES_MAJOR_VERSION=$ES_MAJOR_VERSION
 fi
 
 if [[ ${ES_MAJOR_VERSION} == "2" ]]; then
-  M_LOCK_ALL_SETTING="bootstrap.mlockall"
+    M_LOCK_ALL_SETTING="bootstrap.mlockall"
 elif [[ ${ES_MAJOR_VERSION} -ge 5 ]]; then
-  M_LOCK_ALL_SETTING="bootstrap.memory_lock"
+    M_LOCK_ALL_SETTING="bootstrap.memory_lock"
 else
-  echo "Elasticsearch version ${ES_VERSION} not supported by this script. Aborting!"
-  exit 1
+    echo "Elasticsearch version ${ES_VERSION} not supported by this script. Aborting!"
+    exit 1
 fi
 if [[ $DEBUG ]]; then
-	echo ES_MAJOR_VERSION=$ES_MAJOR_VERSION
+    echo ES_MAJOR_VERSION=$ES_MAJOR_VERSION
 fi
 
 
@@ -166,15 +166,15 @@ BASE=$BASE_PARENT/elastic
 # Check that the user exists
 if ! grep -q "^${ES_USER}:" /etc/passwd; then
     proxy_log "adding user $ES_USER"
-	adduser --disabled-login --system --home $BASE $ES_USER
+    adduser --disabled-login --system --home $BASE $ES_USER
 fi
 
 # sometimes (I'm looking at you, Hetzner) we can find ourselves with a
 # bad IPv6 configuration. If so, disable it here.
 if [[ $DISABLE_IPV6 ]]; then
     proxy_log "disabling ipv6"
-	echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/99-disable-ipv6-all.conf
-	sysctl -p /etc/sysctl.d/99-disable-ipv6-all.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/99-disable-ipv6-all.conf
+    sysctl -p /etc/sysctl.d/99-disable-ipv6-all.conf
 fi
 
 # Find a fallback listening ip for now; probably won't use it
@@ -185,40 +185,40 @@ PRIMARY_IP=${PRIMARY_IP_CIDR%%/*}
 # Find the common member of (slave ip list, our ip list) to listen on
 MY_IPS=( $(ip address list |grep inet|awk '{print $2}'|awk -F/ '{print $1}') )
 for ip in $SLAVE_IPS; do
-  ip=$(bare_ip $ip)
-  for my_ip in $MY_IPS; do
-    if [[ $my_ip == $ip ]]; then
-      PRIMARY_IP=$(bracketed_ip $ip)
-    fi
-  done
+    ip=$(bare_ip $ip)
+    for my_ip in $MY_IPS; do
+        if [[ $my_ip == $ip ]]; then
+            PRIMARY_IP=$(bracketed_ip $ip)
+        fi
+    done
 done
 
 if [[ $DEBUG ]]; then
-	echo SRC_DIR=$SRC_DIR
-	echo TMP_DIR=$TMP_DIR
-	echo BASE=$BASE
+    echo SRC_DIR=$SRC_DIR
+    echo TMP_DIR=$TMP_DIR
+    echo BASE=$BASE
 fi
 
 # Make sure our workspace is clean
 if [[ -d $BASE ]]; then
-	if [[ $SHOVE_BASE ]]; then
-		OLD_BASE=$BASE.$(date --iso-8601=seconds)
+    if [[ $SHOVE_BASE ]]; then
+        OLD_BASE=$BASE.$(date --iso-8601=seconds)
         proxy_log "shove_base"
-		if ! mv $BASE $OLD_BASE; then
-		  echo "Could not move $BASE to $OLD_BASE. Aborting"
-		  exit 1
-		fi
-	else
-		echo "Directory $BASE already exists. Aborting"
-		exit 1
-	fi
+        if ! mv $BASE $OLD_BASE; then
+            echo "Could not move $BASE to $OLD_BASE. Aborting"
+            exit 1
+        fi
+    else
+        echo "Directory $BASE already exists. Aborting"
+        exit 1
+    fi
 fi
 proxy_log "mkdir $BASE"
 if ! mkdir -p $BASE; then
-  echo "Could not create directory $BASE. Aborting"
-  exit 1
+    echo "Could not create directory $BASE. Aborting"
+    exit 1
 else
-  cd $BASE
+    cd $BASE
 fi
 
 
@@ -286,11 +286,11 @@ check_error "apt install"
 SSH_REMOTE_HOST=${SSH_CLIENT%% *}
 
 for ip in $SSH_REMOTE_HOST $CONTROLLER_IP $SLAVE_IPS; do
-  ip=$(bare_ip $ip)
-  for port in $SSH_PORT $ES_PORT $ES_TRANS_PORT; do
-    proxy_log "ufw allow to any port $port from $ip"
-    ufw allow to any port $port from $ip comment "es-constructor"
-  done
+    ip=$(bare_ip $ip)
+    for port in $SSH_PORT $ES_PORT $ES_TRANS_PORT; do
+        proxy_log "ufw allow to any port $port from $ip"
+        ufw allow to any port $port from $ip comment "es-constructor"
+    done
 done
 
 proxy_log "ufw enable"
@@ -326,8 +326,8 @@ mkdir $BASE/elasticsearch-logs
 # first put our slave ip list into json format (QAD)
 SLAVE_IPS_QUOTED_ARRAY=()
 for ip in $SLAVE_IPS; do
-  ip="$(bracketed_ip $ip)"
-  SLAVE_IPS_QUOTED_ARRAY=(${SLAVE_IPS_QUOTED_ARRAY[@]} \"$ip\")
+    ip="$(bracketed_ip $ip)"
+    SLAVE_IPS_QUOTED_ARRAY=(${SLAVE_IPS_QUOTED_ARRAY[@]} \"$ip\")
 done
 
 IFS_SAVE=$IFS
@@ -337,8 +337,8 @@ IFS=$IFS_SAVE
 
 # In DEBUG mode, set the logger level of Elasticsearch to DEBUG too
 if [[ $DEBUG ]]; then
-	cp $ES_BASE/config/log4j2.properties $ES_BASE/config/log4j2.properties.dist
-	sed -i 's/rootLogger\.level = info/rootLogger.level = debug/' $ES_BASE/config/log4j2.properties
+    cp $ES_BASE/config/log4j2.properties $ES_BASE/config/log4j2.properties.dist
+    sed -i 's/rootLogger\.level = info/rootLogger.level = debug/' $ES_BASE/config/log4j2.properties
 fi
 
 mv $ES_BASE/config/elasticsearch.yml $ES_BASE/config/elasticsearch.yml.dist
@@ -361,7 +361,7 @@ EOF
 # For ES 2, we set cache preferences here
 # For ES 5, these are not required with the latest plugin
 if [[ ${ES_MAJOR_VERSION} -lt 5 ]]; then
-	cat >> $ES_BASE/config/elasticsearch.yml <<EOF
+    cat >> $ES_BASE/config/elasticsearch.yml <<EOF
 
 # Vanguard plugin recommended settings
 index.queries.cache.enabled: true
@@ -372,17 +372,17 @@ fi
 
 # only need this if we are using enterprise edition v4
 if [[ $ES_MAJOR_VERSION -lt 5 ]]; then
-  $ES_BASE/$PLUGIN_TOOL install lmenezes/elasticsearch-kopf  || exit 2
-  if [[ -f $SRC_DIR/license-siren-$ES_VERSION.zip ]]; then
-	$ES_BASE/$PLUGIN_TOOL install file:$SRC_DIR/license-siren-$ES_VERSION.zip  || exit 3
-  fi
+    $ES_BASE/$PLUGIN_TOOL install lmenezes/elasticsearch-kopf  || exit 2
+    if [[ -f $SRC_DIR/license-siren-$ES_VERSION.zip ]]; then
+        $ES_BASE/$PLUGIN_TOOL install file:$SRC_DIR/license-siren-$ES_VERSION.zip  || exit 3
+    fi
 fi
 
 # For new ES, we also need to increase the system defaults.
 current_max_map_count=$(sysctl -n vm.max_map_count)
 if [[ $current_max_map_count -lt $MAX_MAP_COUNT ]]; then
-  echo "vm.max_map_count = $MAX_MAP_COUNT" > /etc/sysctl.d/99-elasticsearch.conf
-  sysctl -w "vm.max_map_count=$MAX_MAP_COUNT"
+    echo "vm.max_map_count = $MAX_MAP_COUNT" > /etc/sysctl.d/99-elasticsearch.conf
+    sysctl -w "vm.max_map_count=$MAX_MAP_COUNT"
 fi
 
 ##### END ELASTICSEARCH CONFIGURATION #####
@@ -397,7 +397,7 @@ if [[ $SYSTEMD ]]; then
 
 ##### SYSTEMD CONFIGURATION #####
 
-	cat <<EOF >${ES_BASE}.environment
+    cat <<EOF >${ES_BASE}.environment
 ES_JAVA_OPTS="-Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE $ES_JAVA_OPTS $CUSTOM_ES_JAVA_OPTS"
 EOF
 
@@ -423,9 +423,9 @@ WantedBy=multi-user.target
 Alias=elastic.service
 EOF
 
-	ln -s ../elastic.service /etc/systemd/system/multi-user.target.wants/
-	systemctl daemon-reload
-	systemctl start elastic.service
+    ln -s ../elastic.service /etc/systemd/system/multi-user.target.wants/
+    systemctl daemon-reload
+    systemctl start elastic.service
 
 ##### END SYSTEMD CONFIGURATION #####
 
@@ -433,12 +433,12 @@ else
 
 ##### SUPERVISOR CONFIGURATION #####
 
-	# This is nasty. The only way we can get memlock to be unlimited from
-	# supervisor without rebooting the machine is to run the service as
-	# root, set the limit, and then sudo to the service account. But sudo
-	# will throw away the environment, so...
+    # This is nasty. The only way we can get memlock to be unlimited from
+    # supervisor without rebooting the machine is to run the service as
+    # root, set the limit, and then sudo to the service account. But sudo
+    # will throw away the environment, so...
 
-	cat > /etc/supervisor/conf.d/elastic.conf <<EOF
+    cat > /etc/supervisor/conf.d/elastic.conf <<EOF
 [program:elastic]
 user=root
 directory=$ES_BASE
@@ -446,22 +446,22 @@ command="/usr/local/bin/elastic-unlimiter.sh"
 autorestart=True
 EOF
 
-	cat <<EOF > /usr/local/bin/elastic-unlimiter.sh
+    cat <<EOF > /usr/local/bin/elastic-unlimiter.sh
 #!/bin/bash
 ulimit -l unlimited
 ulimit -n 65536
 sudo -u $ES_USER /usr/local/bin/elastic-launcher.sh
 EOF
-	chmod +x /usr/local/bin/elastic-unlimiter.sh
+    chmod +x /usr/local/bin/elastic-unlimiter.sh
 
-	cat <<EOF > /usr/local/bin/elastic-launcher.sh
+    cat <<EOF > /usr/local/bin/elastic-launcher.sh
 #!/bin/bash
 export ES_JAVA_OPTS="-Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE $ES_JAVA_OPTS $CUSTOM_ES_JAVA_OPTS"
 $ES_BASE/bin/elasticsearch
 EOF
-	chmod +x /usr/local/bin/elastic-launcher.sh
+    chmod +x /usr/local/bin/elastic-launcher.sh
 
-	supervisorctl update
+    supervisorctl update
 
 ##### END SUPERVISOR CONFIGURATION #####
 
@@ -495,5 +495,5 @@ EOF
 
 
 if [[ ! $DEBUG ]]; then
-	rm -rf $TMP_DIR
+    rm -rf $TMP_DIR
 fi
