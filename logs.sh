ad7ef45d34129324ac4850fa934034cdf2906c01
Stéphane Campinas <stephane.campinas@gmail.com>
2018-08-20 11:23:55 +0200

    added comments

--- logs.sh
+++ logs.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+if [[ $1 == "help" || $1 == "-h" || $1 == "--help" ]]; then
+	cat <<EOF
+Usage: $0 <CLUSTER_NAME>
+
+This script retrieves logs from the spawned machines for a particular Elasticsearch cluster.
+The Elasticsearch cluster does not need to be up and running.
+Logs retrieved are syslog and from the elastic systemd service.
+EOF
+fi
+
 cluster_id=$1
 nodes=$(gcloud compute instances list --format "value( 'INTERNAL_IP' )" --filter "NAME ~ ^${cluster_id}-node")
 
