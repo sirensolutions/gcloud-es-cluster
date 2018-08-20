21b6669bb0ad0a20bd22a052fa00068b3d30837b
Stéphane Campinas <stephane.campinas@gmail.com>
2018-08-20 11:37:50 +0200

    check that known_hosts exists

--- spawner.sh
+++ spawner.sh
@@ -202,10 +202,10 @@ for slave in ${SLAVES[@]}; do
 	ip=$(gcloud compute instances describe $slave \
         | grep networkIP | awk '{print $2}')
 	SLAVE_IPS=(${SLAVE_IPS[@]} $ip)
-	# make sure .ssh folder exists
-	mkdir -p "$HOME/.ssh"
 	# Delete this IP from our known_hosts because we know it has been changed
-	ssh-keygen -f "$HOME/.ssh/known_hosts" -R $ip >& /dev/null
+	if [ -f "$HOME/.ssh/known_hosts" ]; then
+		ssh-keygen -f "$HOME/.ssh/known_hosts" -R $ip >& /dev/null
+	fi
 done
 
 # Now that we know all the slave IPs, we can tell the slaves themselves.
