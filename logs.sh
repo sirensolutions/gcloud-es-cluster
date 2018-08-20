15a484ba9b802a7c4b7e0cc41f14624d48b4ac71
Stéphane Campinas <stephane.campinas@gmail.com>
2018-08-20 10:56:46 +0200

    fixed how to read lines from the nodes variable

--- logs.sh
+++ logs.sh
@@ -2,8 +2,8 @@
 
 local nodes=$(gcloud compute instances list --format "value( 'INTERNAL_IP' )" --filter "NAME ~ ^${getClusterId()}-node")
 
-for node in nodes; do
+while read -r node; do
 	local syslog=$(ssh $node grep es-constructor /var/log/syslog)
 	local journal=$(ssh $node journalctl --unit=elastic --no-pager)
 	echo -e "Node ${node}:\n--> syslog:\n${syslog}\n--> journal:\n${journal}"
-done
+done <<< "$nodes"
