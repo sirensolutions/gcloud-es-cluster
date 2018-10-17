485e3049a771b96491c6ec15f89e06262a80bf4d
Andrew Gallagher <andrew.gallagher@siren.io>
2018-10-17 11:56:36 +0100

    Fix quoting

--- baremetal-puller.sh
+++ baremetal-puller.sh
@@ -7,7 +7,7 @@ for arg in $@; do
 	eval $arg
 done
 
-if [[ "$DISABLE_IPV6" && "$DISABLE_IPV6" !="false" ]]; then
+if [[ $DISABLE_IPV6 && "$DISABLE_IPV6" !="false" ]]; then
 	# This is temporary, we'll fix it permanently in the constructor
 	sysctl -w "net.ipv6.conf.all.disable_ipv6=1" |& logger -t es-puller
 fi
