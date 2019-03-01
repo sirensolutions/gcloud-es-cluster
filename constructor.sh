8bc4ec2544f94dbc512a706e55b4b18781f76222
Andrew Gallagher <andrew.gallagher@siren.io>
2019-03-01 15:41:55 +0000

    Force install of software-properties-common

--- constructor.sh
+++ constructor.sh
@@ -230,14 +230,17 @@ popd >/dev/null
 
 ##### DOWNLOAD SOFTWARE #####
 
-add-apt-repository ppa:webupd8team/java
-${ADMIN_TOOLS_DIR}/apt-repo add beats "https://artifacts.elastic.co/packages/6.x/apt stable main" https://artifacts.elastic.co/GPG-KEY-elasticsearch
-
 proxy_log "apt"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 dpkg --configure -a
 apt-get -f install
+apt-get -y install software-properties-common # this provides add-apt-repository
+
+add-apt-repository ppa:webupd8team/java
+${ADMIN_TOOLS_DIR}/apt-repo add beats "https://artifacts.elastic.co/packages/6.x/apt stable main" https://artifacts.elastic.co/GPG-KEY-elasticsearch
+apt-get update # again
+
 # preseed the debian installer with our Java license acceptance
 echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true' | debconf-set-selections
 # make sure the installer does not prompt; there's nobody listening
