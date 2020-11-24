38234b0c2a63c81482fed5dafe48d9b31a665c84
Fabio Corneti <info@corneti.com>
2020-11-24 08:34:11 +0000

    Removed k8s/bench directory

--- k8s/bench/docker/elasticsearch/src/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$1" = 'elasticsearch' ]; then
-	set -- gosu elasticsearch "$@"
-	ES_JAVA_OPTS="-Des.logger.level=$LOG_LEVEL -Xms$HEAP_SIZE -Xmx$HEAP_SIZE"  $@
-else
-	exec $@
-fi
